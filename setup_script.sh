b59cf6d5e9a46fc0b870e1d5a4f566acb3f40d70
Eric Zou <ezou626@gmail.com>
2024-02-22 15:58:53 -0500

    add more comments

--- setup_script.sh
+++ setup_script.sh
@@ -2,13 +2,15 @@
 # sets up conda environment in linux 
 # WARNING: HAS NOT BEEN TESTED, SORRY! PLEASE LMK
 # IF YOU TRIED THIS AND IT DOESN'T WORK
+
+# create venv
 python -m venv .neufa_finetune
 source .venv/bin/activate
-pip install torch
+
+# install dependencies
+pip install torch torchaudio
 pip install pandas numpy matplotlib
 pip install librosa 
-pip install tqdm
-pip install torchaudio
-pip install jupyter
+pip install jupyter tqdm
 pip install swig sequitur-g2p
 git clone --recursive https://github.com/petronny/g2p
